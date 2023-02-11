bee3fadd59bef45e005a530b1e6705c15976790f
Denise Ignatova <denis.ignatova@re-flow.co.uk>
2023-02-11 17:51:42 +0000

    cors2

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://blogs-black-one.vercel.app/api/:path*',
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
